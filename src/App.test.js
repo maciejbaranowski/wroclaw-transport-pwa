05bb8e005bd32d5fa19f08e865d767fab1218d43
Maciej Baranowski <maciekbaranowski@onet.pl>
2020-07-21 08:49:17 +0200

    test cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('dummy test', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/MPK/i);
   expect(linkElement).toBeInTheDocument();
 });
